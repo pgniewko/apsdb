3c4ef82481d5ea7d2fa80827775d524239e7c58a
Pawel Gniewek <gniewko.pablo@gmail.com>
2017-07-22 13:14:38 -0700

    Local version of the countryinfo library

--- geo_plots.py
+++ geo_plots.py
@@ -5,46 +5,60 @@ import numpy as np
 import pandas as pd
 from pandas import DataFrame
 import matplotlib.pylab as plt
+import geopandas as gpd
+import geograpy
+
+import countryinfo.countryinfo as countryinfo
+
+#def replace_char(ch1, ch2):
+    
 
 if __name__ == "__main__":
 
     df = pd.read_table(sys.argv[1], delim_whitespace=True, names=( 'doi', 'journal', 'year', 'country'), comment='#', header=None )
+     
 
-#    print df.head()
-
-#    print df.columns
-
-
-#    print df.describe()
- 
-    dat_ = df.groupby(['year','country']).size()
-
-    df['COUNTER'] =1       #initially, set that counter to 1.
-    group_data = df.groupby(['year','country'])['COUNTER'].sum() #sum function
-    print group_data
-
-#    print dat_.keys().shape
-#    print dir(dat_)
-#    print dat_['year']
-#    plt.plot( dat_ )
-#    plt.show()
-
-    print dat_['2016']
-
-    sys.exit(1)
-    df.index.name = 'ID'
-
-    print df.columns
-    print df.__doc__
-    print df.index
-    print df.iloc[12]['year']
-
-
-    df = df.cumsum('year')
-    plt.figure()
-    df.plot()
-    plt.show()
-
-
+    dat_ = df.groupby(['country']).size().sort_values(ascending=False)
+  
+    as_list = dat_.index.tolist()
     
+    for ix, it_ in enumerate(as_list):
+        as_list[ix] = it_.replace('.',' ')
+        
+#        places = geograpy.get_place_context(text=unicode(as_list[ix]))
+        
+#        print as_list[ix], places.countries
+
+    dat_.index = as_list
+
+    world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+    cities = gpd.read_file(gpd.datasets.get_path('naturalearth_cities'))
+
+#    print cities['name']
+  
+    print 'Warsaw' in cities['name'].tolist()
+    countries = countryinfo.countries
+
+    t1=[]
+    t2=[]
+    t3=[]
+    for c in countries:
+        if c['name'] in  dat_.index:
+            try:
+                ix = cities['name'].tolist().index(c['capital'])
+                print ix, c['name'], c['capital']
+            #coordinates = cities['geometry'][ix]
+            except ValueError:
+                print c['capital'], " was not found."
+                continue
+
+            #print c['name'], c['capital'], coordinates
+            t1.append( c['name'] )
+            t2.append( c['capital']  )
+            
+
+    d = {'Name':t1,'Capital':t2}
+    df_capitals = pd.DataFrame(data=d)
+
+
 
