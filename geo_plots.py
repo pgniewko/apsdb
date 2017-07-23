912da21ee7a786c5ceaea86dd210d72ba0e0883c
Pawel Gniewek <gniewko.pablo@gmail.com>
2017-07-22 20:27:40 -0700

    Plotting map done

--- geo_plots.py
+++ geo_plots.py
@@ -7,10 +7,8 @@ from pandas import DataFrame
 import matplotlib.pylab as plt
 import geopandas as gpd
 import geograpy
+#import countryinfo.countryinfo as countryinfo
 
-import countryinfo.countryinfo as countryinfo
-
-#def replace_char(ch1, ch2):
 
 corrections = { \
 "Moldova, Republic of":'Moldova',\
@@ -28,14 +26,25 @@ corrections = { \
 "Bosnia and Herzegovina":"Bosnia and Herz.",\
 "Macedonia, Republic of":"Macedonia",\
 "Venezuela, Bolivarian Republic of":"Venezuela",\
-"Dominican Republic":"Dominican Rep."
+"Dominican Republic":"Dominican Rep.",\
+"Taiwan, Province of China":"China",\
+"Tanzania, United Republic of":"Tanzania",\
+"Saint Martin (French part)":"France",\
+"French Southern Territories":"France",\
+"Congo, The Democratic Republic of the":"Congo",\
+"Saint Pierre and Miquelon":"France",\
+"Saint Helena, Ascension and Tristan da Cunha":"United Kingdom"
 }
 
 if __name__ == "__main__":
 
+    TOTAL_NUMBER = 0
+    REGISTERED_NUMBER = 0
     df = pd.read_table(sys.argv[1], delim_whitespace=True, names=( 'doi', 'journal', 'year', 'country'), comment='#', header=None )
     dat_ = df.groupby(['country']).size().sort_values(ascending=False)
  
+    TOTAL_NUMBER = dat_.sum()
+
     as_list = dat_.index.tolist()
     for ix, it_ in enumerate(as_list):
         as_list[ix] = it_.replace('.',' ')
@@ -43,11 +52,14 @@ if __name__ == "__main__":
 
     
     world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+    
+    # Extra field to store the number of papers
+    world['papers'] = 0.0 * world.gdp_md_est
 
 
     cities = gpd.read_file(gpd.datasets.get_path('naturalearth_cities'))
 
-    countries = countryinfo.countries
+#    countries = countryinfo.countries
 
     t1=[]
     t2=[]
@@ -55,14 +67,7 @@ if __name__ == "__main__":
     t4=[]     
     t5=[]
     t6=[]
-    t7=[]
-    t8=[]
 
-    for cc_ in world['name']:
-        print cc_
-
-    world['papers'] = 0.0 * world.gdp_md_est
-#    for c in countries:
     for c in dat_.index:
         try:
             cc = corrections[c]
@@ -71,39 +76,52 @@ if __name__ == "__main__":
 
         if cc in world['name'].tolist():
             try:
-                #ix = cities['name'].tolist().index(c['capital'])
-                #ix_w = world['name'].tolist().index( c['name'] )
                 ix_w = world['name'].tolist().index( cc )
             except ValueError:
-                print c, cc, " was not Found in world name "
+                print cc, " [",c,"]", " was not Found in world name."
                 continue
            
-            #print ix, ix_w, c['name'], world['continent'][ix_w]
-#            t1.append( c['name'] )
             t1.append( c )
-            t2.append( "" ) #c['capital']  )
-            t3.append( "" ) # cities['geometry'][ix] )
-#            t4.append( dat_.values[ dat_.index.tolist().index( c['name'] ) ] )
-            t4.append( dat_.values[ dat_.index.tolist().index( c ) ] )
+            t2.append( dat_.values[ dat_.index.tolist().index( c ) ] )
 
-            t5.append( world['geometry'][ix_w] )
-            t6.append( world['continent'][ix_w] )
-            t7.append( world['pop_est'][ix_w] )
-            t8.append( world['gdp_md_est'][ix_w] )
-            world['papers'][ix_w] += ( dat_.values[ dat_.index.tolist().index( c ) ] )**1.0
+            t3.append( world['geometry'][ix_w] )
+            t4.append( world['continent'][ix_w] )
+            t5.append( world['pop_est'][ix_w] )
+            t6.append( world['gdp_md_est'][ix_w] )
+            world['papers'][ix_w] += (dat_.values[ dat_.index.tolist().index( c ) ] * 1000.0)
+            REGISTERED_NUMBER += dat_.values[ dat_.index.tolist().index( c ) ]
         else:
-            print "Not processed:", cc, cc in world['name'].tolist()
+            REGISTERED_NUMBER += dat_.values[ dat_.index.tolist().index( c ) ]
+            print "Not processed (not in geopandas):", cc, ".", "Number of citations missed= ", dat_.values[ dat_.index.tolist().index( c ) ]
+
+
+    print "TOTAL_NUMBER= ", TOTAL_NUMBER, " REGISTERED_NMBER= ", REGISTERED_NUMBER
+
 
     world['papers_per_capita'] = world.papers / world.pop_est
     world['papers_per_gdp'] = world.papers / world.gdp_md_est
 
-    d = {'name':t1,'capital':t2, 'point':t3, 'counts':t4, 'geometry':t5, 'continent':t6, 'pop_est': t7, 'gdp_md_est': t8}
+    d = {'name':t1, 'counts':t2, 'geometry':t3, 'continent':t4, 'pop_est': t5, 'gdp_md_est': t6}
+    # My custom data frame
     df_mydata = gpd.GeoDataFrame(data=d)
 
     # Remove Antarctica so the map looks nicer:
     world = world[(world.name != "Antarctica") & (world.name != "Fr. S. Antarctic Lands") & (world.name != "Greenland")]
-    world = world.to_crs({'init': 'epsg:3395'}) # world.to_crs(epsg=3395) would also work
-    base = world.plot(column='papers_per_capita', cmap='rainbow')
 
-    plt.title('Number of APS papers per capita', fontsize=20)
+    fig, ax = plt.subplots(1, figsize=(14,7))
+    vmin, vmax = 0, np.array(world['papers_per_capita']).max()
+    base = world.plot(ax=ax, column='papers_per_capita', cmap='rainbow', vmin=vmin, vmax=vmax)
+
+#    
+    plt.title('Number of APS papers per 1000 citizens', fontsize=20)
+    plt.xlabel('Latitude [$^\circ$]', fontsize=15)
+    plt.ylabel('Longitude [$^\circ$]', fontsize=15)
+#
+
+    cax = fig.add_axes([0.92, 0.12, 0.03, 0.7])
+    sm = plt.cm.ScalarMappable(cmap='rainbow', norm=plt.Normalize(vmin=vmin, vmax=vmax))
+    # fake up the array of the scalar mappable. Urgh...
+    sm._A = []
+    fig.colorbar(sm, cax=cax)
+
     plt.show()
