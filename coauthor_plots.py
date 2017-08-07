b0ba5b39d316a9f1098653a444b340e6d1b061c4
Pawel Gniewek <gniewko.pablo@gmail.com>
2017-08-06 23:45:23 -0700

    Plotting max number of authors

--- coauthor_plots.py
+++ coauthor_plots.py
@@ -13,17 +13,24 @@ if __name__ == "__main__":
 
     dat_ = df.groupby(['journal','year'])['nauthors'].mean()
     means  = df.groupby(['year'])['nauthors'].mean()
+    max_  = df.groupby(['year'])['nauthors'].max()
     errors = df.groupby(['year'])['nauthors'].std()
     
 
 
     fig, (ax1, ax2) = plt.subplots(1, 2, sharey=True, figsize=(14,7) )
    
-    means.plot(yerr=errors, ax=ax1, label='All APS journals')
+    ax3 = ax1.twinx()
+    means.plot(yerr=errors, ax=ax1, label='All APS journals',color='blue')
+    max_.plot(ax=ax3,color='green',linewidth=1.5, label='Max number of authors')
     ax1.set_xlabel('Year', fontsize=20)
-    ax1.set_ylabel('Number of authors', fontsize=20)
+    ax1.set_ylabel('Number of authors', fontsize=20, color='blue')
     ax1.grid(linestyle='--', linewidth=1)
-    ax1.legend(numpoints=1, loc=0, fontsize=15)
+    ax1.tick_params('y',colors='blue')
+    ax3.set_ylabel('Max number of authors', color='green', fontsize=20)
+    ax3.tick_params('y', colors='green')
+    ax1.legend(numpoints=1, loc=2, fontsize=15)
+    ax3.legend(numpoints=1, loc=4, fontsize=15)
 
     
 
@@ -34,6 +41,7 @@ if __name__ == "__main__":
     means_prd = dat_['PRD']
     means_pre = dat_['PRE']
     
+    ax4 = ax2.twinx()
     means_prl.plot(ax=ax2,label="PRL")
     means_pr.plot(ax=ax2, label="PR")
     means_pra.plot(ax=ax2, label="PRA")
@@ -43,22 +51,12 @@ if __name__ == "__main__":
     
     
     ax2.set_xlabel('Year', fontsize=20)
-    ax2.set_ylabel('Number of authors', fontsize=20)
+    ax4.set_ylabel('Number of authors', color='blue',fontsize=20)
     ax2.grid(linestyle='--', linewidth=1)
     ax2.legend(numpoints=1, loc=0, fontsize=15)
-    
+    ax4.axes.get_yaxis().set_ticks([])
     fig.text(0.95, 0.05, '(c) 2017, P.G.',fontsize=10, color='gray', ha='right', va='bottom', alpha=0.5)
 
     plt.show()
 
-#    means = df.groupby(['year'])['nauthors'].mean()
-#    errors = df.groupby(['year'])['nauthors'].std()
-
-
-    sys.exit(1)
-
-    fig, ax = plt.subplots()
-    means.plot(yerr=errors, ax=ax)
-    plt.show()
-  
 
