a605ecdef9d4e30a143f23c4acfb272e9544ee98
Pawel Gniewek <gniewko.pablo@gmail.com>
2018-03-11 15:17:52 -0700

    Removed scholar-dependent functions

--- utils.py
+++ utils.py
@@ -2,7 +2,6 @@
 
 import numpy as np
 import json
-from scholar.scholar import *
 
 failed_tries = 0
 
@@ -121,28 +120,3 @@ def get_all_affiliations(json_file_):
     return aff_
 
 
-def get_number_citations(doi_):
-#    ScholarConf.LOG_LEVEL = 4    
-    querier  = ScholarQuerier()
-    query    = SearchScholarQuery()
-    query.set_phrase(doi_)
-    querier.send_query(query)
-
-    if len(querier.articles) > 0:
-        try: 
-           items = sorted(list(querier.articles[0].attrs.values()),key=lambda item: item[2])
-        except IndexError:
-            return -1
-    else:
-        return -1
-
-    cits_ = -1
-
-    for item in items:
-        if item[1] == 'Citations':
-            cits_ = item[0]
-        
-        if item[1] == 'Versions list':
-            cit_link_ = item[0]
-
-    return cits_, cit_link_
