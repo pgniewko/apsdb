e24fc4688a9b8826de05c33e4024cce20148dc4e
Pawel Gniewek <gniewko.pablo@gmail.com>
2018-04-10 12:56:22 -0700

    File renamed and cleaned

--- citations_network.py
+++ citations_network.py
@@ -1,25 +1,18 @@
 #! /usr/bin/env python
 
-import numpy as np
-import matplotlib.pylab as plt
-
-
 from utils import parse_csv_file
 
-
-def build_network(csv_file):
-
+def build_graph(csv_file):
     dict_1, dict_2 = parse_csv_file(csv_file)
-    return  
-
+    
 
 
 if __name__ == "__main__":
     
     citations_path = '../data/aps-dataset-citations-2016/aps-dataset-citations-2016.csv'
     
-    build_network(itations_path)
+    build_graph(citations_path)
 
-
-
+    
+    
 
