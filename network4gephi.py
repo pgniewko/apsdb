a147f984fa87c5f0dea0882e3e28d9e5b7a61d51
Pawel Gniewek <gniewko.pablo@gmail.com>
2018-04-15 12:14:41 -0700

    GEXF file bugfix

--- network4gephi.py
+++ network4gephi.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
     citations_path = '../data/aps-dataset-citations-2016/aps-dataset-citations-2016.csv'
 #    citations_path = '../data/aps-dataset-citations-2016/aps-dataset-citations-2016_MOCK.csv'
 
-    ograph = '../results/citations_graph.gefx'
+    ograph = '../results/citations_graph.gexf'
 
     G = build_graph(citations_path, xmin=60)
     nx.info(G)
