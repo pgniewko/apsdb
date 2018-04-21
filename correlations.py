27d23ad0c4cad418332437f45bf8529e48f99f4f
Pawel Gniewek <gniewko.pablo@gmail.com>
2018-04-20 20:54:42 -0700

    Correlations calc from random sampling

--- correlations.py
+++ correlations.py
@@ -16,8 +16,8 @@ if __name__ == "__main__":
     
     database_path = '../db_text/papers_data.txt'
 
-    journals = ['PRA','PRB','PRC','PRD','PRE','PRL']
-    journals = ['PRA','PRAPPLIED']
+    journals = ['PRA','PRB','PRC','PRD','PRE','PRL','RMP']
+    #journals = ['PRA','PRAPPLIED']
 
     
 
@@ -25,28 +25,38 @@ if __name__ == "__main__":
     names=( 'year','month','day','journal','issue','volume','doi','coauts','affs','countries','titlen','numpages','cits','refs'), \
     comment='#', header=None )
 
-
     cats = ['year','month','day','issue','volume','coauts','affs','countries','titlen','numpages','cits','refs']
-    j_colors = {'PRA':'blue','PRB':'green','PRC':'red','PRD':'orange','PRE':'black','PRL':'magenta','PRAPPLIED':'red'}
+    j_colors = {'PRA':'blue','PRB':'green','PRC':'red','PRD':'orange','PRE':'black','PRL':'magenta','RMP':'red'}
 
     fout = open('../results/correlations.dat','w')
     
-
+    nsamples = 5
     fig, ax1 = plt.subplots(1, 1, sharey=True, figsize=(7,7) )
     for j_ in journals:
         pearson_corr = []
         dist_corr = []
         for i,el_i in enumerate(cats):
             for j,el_j in enumerate(cats):
-                if i > j:
+                if j > i:
                     df_j = df[df['journal'] == j_]
-                    new_data = df_j[[el_i,el_j]]._get_numeric_data()
+                    new_data = df_j[ [el_i,el_j] ]#._get_numeric_data()
                     new_data = new_data.dropna()
 
-                    data_i = new_data[el_i]
-                    data_j = new_data[el_j]
-                    r,p = pearsonr(data_i,data_j)
-                    covxy, corrxy, varx, vary = distance_stats(data_i, data_j)
+                    r = 0.0
+                    corrxy = 0.0
+                    for i in range(nsamples):
+                        sample_size = min(2500, len(new_data[el_i]))
+                        new_data_sample = new_data.sample( sample_size )
+                        data_i = pd.to_numeric( new_data_sample[el_i] )
+                        data_j = pd.to_numeric( new_data_sample[el_j] )
+                        ri,pi = pearsonr(data_i,data_j)
+                        covxyi, corrxyi, varxi, varyi = distance_stats(data_i, data_j)
+                   
+                        r += ri
+                        corrxy += corrxyi
+
+                    r /= nsamples
+                    corrxy /= nsamples
                     pearson_corr.append(r)
                     dist_corr.append(corrxy)
                     s = "%10s  %10s  %10s  % f  % f \n" % ( j_, el_i, el_j, r, corrxy)
