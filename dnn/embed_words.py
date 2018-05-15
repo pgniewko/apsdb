69379f9eb6cb009ae4b625f48a637ae0c2848e87
Pawel Gniewek <gniewko.pablo@gmail.com>
2018-05-15 11:33:26 -0700

    many iterations added

--- dnn/embed_words.py
+++ dnn/embed_words.py
@@ -20,12 +20,8 @@ if __name__ == "__main__":
 
     SIZES=[100,200,300]
     WINDOWS=[5,10,15]
-    MIN_COUNTS=[2,5,10]
+    MIN_COUNTS=[1,5,10,25]
     
-#    SIZES=[300]
-#    WINDOWS=[10]
-#    MIN_COUNTS=[5]
-   
     print("READING SENTENCES ...")
     sentences = read_sentences( SENTENCES_FILE )
     print("Number of sentences=%d" %( len(sentences) ) )
@@ -33,26 +29,25 @@ if __name__ == "__main__":
     for SIZE in SIZES:
       for WINDOW in WINDOWS:
         for MIN_COUNT in MIN_COUNTS:
-            print("EMBEDDING WORDS: size=%d, window=%d, min_count=%d" % (SIZE, WINDOW, MIN_COUNT) )
-            model = Word2Vec(sentences, min_count=MIN_COUNT, size=SIZE, window=WINDOW,iter=1,sg=1)
-            print("EMBEDDING DONE !")
-
+          for ITER in ITERS:
+            print("EMBEDDING WORDS: size=%d, window=%d, min_count=%d, iter=%d" % (SIZE, WINDOW, MIN_COUNT,ITER) )
+            model = Word2Vec(sentences, min_count=MIN_COUNT, size=SIZE, window=WINDOW,iter=ITER,sg=1)
             print model
-            print model.most_similar('atom')
-    
-            model.save(opath+'Abstracts_Word2Vec_SIZE_'+str(SIZE)+'_WINDOW_'+str(WINDOW)+"_MIN_COUNT_"+str(MIN_COUNT)+".bin")
-  
+            print("EMBEDDING DONE !")
+            model.save(opath+'Abstracts_Word2Vec_SIZE_'+str(SIZE)+'_WINDOW_'+str(WINDOW)+"_MIN_COUNT_"+str(MIN_COUNT)+"_ITER_"+str(ITER)+".bin")
+ 
+
     print("ALL EMBEDDINGS ARE CREATED")
 
     f, (ax1, ax2) = plt.subplots(1, 2, figsize=(14,7))
-    model = Word2Vec.load(opath+'Abstracts_Word2Vec_SIZE_300_WINDOW_10_MIN_COUNT_5.bin')
+    model = Word2Vec.load(opath+'Abstracts_Word2Vec_SIZE_200_WINDOW_3_MIN_COUNT_10_ITER_1.bin')
     X = model[model.wv.vocab]
     
 
     tsne = TSNE(n_components=2)
     X_tsne = tsne.fit_transform(X)
     ax1.scatter(X_tsne[:, 0], X_tsne[:, 1])
-    ax1.set_title('t-SNE (size=%d;window=%d,min_count=%d' %(300,10,5) )
+    ax1.set_title('t-SNE (size=%d;window=%d,min_count=%d)' %(200,5,5) )
     ax1.set_xlabel(r'$\rm X_1$', fontsize=30)
     ax1.set_ylabel(r'$\rm X_2$', fontsize=30)
 
@@ -60,7 +55,7 @@ if __name__ == "__main__":
     pca = PCA(n_components=2)
     X_pca = pca.fit_transform(X)
     ax2.scatter(X_pca[:, 0], X_pca[:, 1])
-    ax2.set_title('PCA (size=%d;window=%d,min_count=%d)' %(300,10,5) )
+    ax2.set_title('PCA (size=%d;window=%d,min_count=%d)' %(200,5,5) )
     ax2.set_xlabel(r'$\rm X_1$', fontsize=30)
     ax2.set_ylabel(r'$\rm X_2$', fontsize=30)
 
